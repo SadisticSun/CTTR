0b5f379d6a6e8a1e6e3781d872c2e980c26b862b
SadisticSun <tristan@iamt.nl>
2017-02-28 14:42:22 +0100

    Navigation changes

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 (function() {
     var _toggleNav          = document.getElementById('navToggle'),
+        _navLink            = document.querySelectorAll('main-navigation-links a'),
         _toggleDropdown     = document.getElementById('dropdownToggle'),
         _navList            = document.getElementById('main-navigation-links'),
         _dropdown           = document.getElementById('dropdown-list');
+        _progress           = document.getElementById('progress');
+        _forms              = document.getElementById('forms');
+        _articles           = document.getElementById('articles');
+        _rating             = document.getElementById('rating');
+        _sections           = document.querySelectorAll('section');
+
 
     function toggleHandler(toggle, nav) {
         toggle.addEventListener("click", function() {
