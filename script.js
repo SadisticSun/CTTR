78c02faa220f9c414991ba845096844362818d55
SadisticSun <tristan@iamt.nl>
2017-03-01 17:33:42 +0100

    Changed stuff

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
 (function() {
-    var _toggleNav          = document.getElementById('navToggle'),
-        _navLink            = document.querySelectorAll('main-navigation-links a'),
-        _toggleDropdown     = document.getElementById('dropdownToggle'),
-        _navList            = document.getElementById('main-navigation-links'),
-        _dropdown           = document.getElementById('dropdown-list');
-        _progress           = document.getElementById('progress');
-        _forms              = document.getElementById('forms');
-        _articles           = document.getElementById('articles');
-        _rating             = document.getElementById('rating');
-        _sections           = document.querySelectorAll('section');
 
-
-    function toggleHandler(toggle, nav) {
-        toggle.addEventListener("click", function() {
-            nav.classList.toggle('hide');
-        })
+    function $(id) {
+        return document.querySelector(id);
     }
-    toggleHandler(_toggleNav, _navList);
-    toggleHandler(_toggleDropdown, _dropdown)
+    function $$(classNames) {
+        return document.querySelectorAll(classNames);
+    }
+
+
+    var DOMelements = {
+        _toggleNav          : $('#navToggle')
+        _navLink            : $$('main-navigation-links a'),
+        _toggleDropdown     : $('#dropdownToggle'),
+        _navList            : $('#main-navigation-links'),
+        _dropdown           : $('#dropdown-list'),
+        _progress           : $('#progress'),
+        _forms              : $('#forms'),
+        _articles           : $('#articles'),
+        _rating             : $('#rating'),
+        _sections           : $$('section')
+    };
+
+    var toggleElements = {
+        progressToggle      : $('#progressToggle'),
+        formsToggle         : $('#formsToggle'),
+        mediaItemToggle     : $('#mediaItemToggle'),
+        titleImageToggle    : $('#titleImageToggle'),
+        fullImageToggle     : $('#fullImageToggle')
+        ratingToggle        : $('#ratingToggle')
+    };
+
+    function toggleHandler(toggle, el) {
+        toggle.addEventListener("click", function() {
+            el.classList.toggle('hide');
+        })
+    };
+
+
+
+
+    toggleHandler(DOMelements._toggleNav, DOMelements._navList);
+    toggleHandler(DOMelements._toggleDropdown, DOMelements._dropdown);
+
+    toggleHandler(toggleElements.progressToggle, DOMelements._progress);
+    toggleHandler(toggleElements.formsToggle, DOMelements._forms);
+    toggleHandler(toggleElements.ratingToggle, DOMelements._rating);
+    toggleHandler(toggleElements.mediaItemToggle, DOMelements._mediaItem);
+    toggleHandler(toggleElements.titleImageToggle, DOMelements._titleImage);
+    toggleHandler(toggleElements.fullImageToggle, DOMelements._fullImage);
 
 })();
